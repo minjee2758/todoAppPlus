f34f0fd4e2c3ae36e5906e80937d5c260a3df0fe
minjee2758 <minjee2758@naver.com>
2025-03-31 10:32:41 +0900

    [feat] 일정관리앱 생성

---
+++ src/test/java/com/example/todoappplus/TodoAppPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
